d262e09eb940084fdf2199a57bfcc6b27ac49284
BakaFT <bakaft@outlook.com>
2023-08-07 12:44:25 +0800

    Wait it's working?

--- plugins/src/preload/api/Fs.ts
+++ plugins/src/preload/api/Fs.ts
@@ -3,36 +3,23 @@ import native from './native';
 window.Fs = {
     readFile(path: string) {
         // ignore top-level js
-        const pluginEntry = new Error().stack?.match(/(?:http|https):\/\/[^\s]+\.js/g)?.[0]
-        const match = pluginEntry?.match(/\/([^/]+)\/index\.js$/)
-        const pluginName = match ? match[1] : null;
-
+        const pluginName = getScriptPath()?.match(/\/([^/]+)\/index\.js$/)?.[1]
         return native.ReadFile(pluginName + "/" + path);
     },
     readFileAsync(path: string) {
         return new Promise<string | undefined>((resolve) => {
-            const pluginEntry = new Error().stack?.match(/(?:http|https):\/\/[^\s]+\.js/g)?.[0]
-            const match = pluginEntry?.match(/\/([^/]+)\/index\.js$/)
-            const pluginName = match ? match[1] : null;
-
+            const pluginName = getScriptPath()?.match(/\/([^/]+)\/index\.js$/)?.[1]
             const result = native.ReadFile(pluginName + "/" + path);
             resolve(result);
-
         });
     },
     writeFile(path: string, content: string, enableAppendMode: boolean) {
-        const pluginEntry = new Error().stack?.match(/(?:http|https):\/\/[^\s]+\.js/g)?.[0]
-        const match = pluginEntry?.match(/\/([^/]+)\/index\.js$/)
-        const pluginName = match ? match[1] : null;
-
+        const pluginName = getScriptPath()?.match(/\/([^/]+)\/index\.js$/)?.[1]
         return native.WriteFile(pluginName + "/" + path, content, enableAppendMode);
     },
     writeFileAsync(path: string, content: string, enableAppendMode: boolean) {
         return new Promise<boolean>((resolve) => {
-            const pluginEntry = new Error().stack?.match(/(?:http|https):\/\/[^\s]+\.js/g)?.[0]
-            const match = pluginEntry?.match(/\/([^/]+)\/index\.js$/)
-            const pluginName = match ? match[1] : null;
-
+            const pluginName = getScriptPath()?.match(/\/([^/]+)\/index\.js$/)?.[1]
             const result = native.WriteFile(pluginName + "/" + path, content, enableAppendMode);
             resolve(result);
         });
